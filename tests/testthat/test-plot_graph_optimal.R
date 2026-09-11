53d285ad7e288fb035f20c7cc757947804b6b3a5
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-09-11 09:58:15 +0100

    test plot_graph_optimal sans snapshots

--- tests/testthat/test-plot_graph_optimal.R
+++ tests/testthat/test-plot_graph_optimal.R
@@ -164,8 +164,24 @@ test_that("create_edges() works", {
 
     edges_df <- create_edges(trans_matrix)
 
-    expect_snapshot(
-        create_edges(trans_matrix)
+    expect_equal(
+        create_edges(trans_matrix),
+        tibble::tribble(
+            ~from , ~to  , ~value ,
+            "H1"  , "H2" , 0.098  ,
+            "H1"  , "H3" , 0.439  ,
+            "H1"  , "H4" , 0.462  ,
+            "H2"  , "H1" , 0.488  ,
+            "H2"  , "H3" , 0.465  ,
+            "H2"  , "H4" , 0.045  ,
+            "H3"  , "H1" , 0.192  ,
+            "H3"  , "H2" , 0.165  ,
+            "H3"  , "H4" , 0.642  ,
+            "H4"  , "H1" , 0.232  ,
+            "H4"  , "H2" , 0.466  ,
+            "H4"  , "H3" , 0.301
+        ),
+        tolerance = 1e-2
     )
 
     expect_s3_class(
@@ -206,11 +222,19 @@ test_that("create_nodes() works", {
 
     nodes <- create_nodes(hyp_weight, edges = edges_df)
 
-    expect_snapshot(
+    expect_equal(
         create_nodes(
             hyp_weight,
             edges = edges_df
-        )
+        ),
+        tibble::tribble(
+            ~hypothesis , ~weight , ~optimised ,
+            "H1"        , 0.125   , TRUE       ,
+            "H2"        , 0.176   , TRUE       ,
+            "H3"        , 0.270   , TRUE       ,
+            "H4"        , 0.429   , TRUE
+        ),
+        tolerance = 1e-2
     )
 
     expect_s3_class(
